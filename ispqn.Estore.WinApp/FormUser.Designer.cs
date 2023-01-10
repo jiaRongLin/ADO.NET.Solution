b1cefe3bcef44ecdb88f19cc91a36a245d5da931
Jia <vernalin1422@gmail.com>
2023-01-10 13:32:43 +0800

    1/10

--- ispqn.Estore.WinApp/FormUser.Designer.cs
+++ ispqn.Estore.WinApp/FormUser.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.label1 = new System.Windows.Forms.Label();
 			this.txtName = new System.Windows.Forms.TextBox();
 			this.label2 = new System.Windows.Forms.Label();
@@ -37,7 +38,16 @@
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
 			this.buttonSearch = new System.Windows.Forms.Button();
 			this.buttonAdd = new System.Windows.Forms.Button();
+			this.userEntityBindingSource = new System.Windows.Forms.BindingSource(this.components);
+			this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.accountDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.passwordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.dateOfBirthDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.heightDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.userEntityBindingSource)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -90,7 +100,17 @@
 			// 
 			// dataGridView1
 			// 
+			this.dataGridView1.AutoGenerateColumns = false;
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.accountDataGridViewTextBoxColumn,
+            this.passwordDataGridViewTextBoxColumn,
+            this.dateOfBirthDataGridViewTextBoxColumn,
+            this.heightDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn});
+			this.dataGridView1.DataSource = this.userEntityBindingSource;
 			this.dataGridView1.Location = new System.Drawing.Point(36, 81);
 			this.dataGridView1.Name = "dataGridView1";
 			this.dataGridView1.RowHeadersWidth = 51;
@@ -119,6 +139,69 @@
 			this.buttonAdd.UseVisualStyleBackColor = true;
 			this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
 			// 
+			// userEntityBindingSource
+			// 
+			this.userEntityBindingSource.DataSource = typeof(Ispan.Estore.BLL.Core.UserEntity);
+			// 
+			// idDataGridViewTextBoxColumn
+			// 
+			this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+			this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+			this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
+			this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+			this.idDataGridViewTextBoxColumn.Width = 125;
+			// 
+			// nameDataGridViewTextBoxColumn
+			// 
+			this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+			this.nameDataGridViewTextBoxColumn.HeaderText = "姓名";
+			this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+			this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+			this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+			this.nameDataGridViewTextBoxColumn.Width = 125;
+			// 
+			// accountDataGridViewTextBoxColumn
+			// 
+			this.accountDataGridViewTextBoxColumn.DataPropertyName = "Account";
+			this.accountDataGridViewTextBoxColumn.HeaderText = "帳號";
+			this.accountDataGridViewTextBoxColumn.MinimumWidth = 6;
+			this.accountDataGridViewTextBoxColumn.Name = "accountDataGridViewTextBoxColumn";
+			this.accountDataGridViewTextBoxColumn.ReadOnly = true;
+			this.accountDataGridViewTextBoxColumn.Width = 125;
+			// 
+			// passwordDataGridViewTextBoxColumn
+			// 
+			this.passwordDataGridViewTextBoxColumn.DataPropertyName = "Password";
+			this.passwordDataGridViewTextBoxColumn.HeaderText = "密碼";
+			this.passwordDataGridViewTextBoxColumn.MinimumWidth = 6;
+			this.passwordDataGridViewTextBoxColumn.Name = "passwordDataGridViewTextBoxColumn";
+			this.passwordDataGridViewTextBoxColumn.ReadOnly = true;
+			this.passwordDataGridViewTextBoxColumn.Width = 125;
+			// 
+			// dateOfBirthDataGridViewTextBoxColumn
+			// 
+			this.dateOfBirthDataGridViewTextBoxColumn.DataPropertyName = "DateOfBirth";
+			this.dateOfBirthDataGridViewTextBoxColumn.HeaderText = "生日";
+			this.dateOfBirthDataGridViewTextBoxColumn.MinimumWidth = 6;
+			this.dateOfBirthDataGridViewTextBoxColumn.Name = "dateOfBirthDataGridViewTextBoxColumn";
+			this.dateOfBirthDataGridViewTextBoxColumn.Width = 125;
+			// 
+			// heightDataGridViewTextBoxColumn
+			// 
+			this.heightDataGridViewTextBoxColumn.DataPropertyName = "Height";
+			this.heightDataGridViewTextBoxColumn.HeaderText = "身高";
+			this.heightDataGridViewTextBoxColumn.MinimumWidth = 6;
+			this.heightDataGridViewTextBoxColumn.Name = "heightDataGridViewTextBoxColumn";
+			this.heightDataGridViewTextBoxColumn.Width = 125;
+			// 
+			// emailDataGridViewTextBoxColumn
+			// 
+			this.emailDataGridViewTextBoxColumn.DataPropertyName = "Email";
+			this.emailDataGridViewTextBoxColumn.HeaderText = "Email";
+			this.emailDataGridViewTextBoxColumn.MinimumWidth = 6;
+			this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+			this.emailDataGridViewTextBoxColumn.Width = 300;
+			// 
 			// FormUser
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
@@ -136,6 +219,7 @@
 			this.Name = "FormUser";
 			this.Text = "FormUser";
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.userEntityBindingSource)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -152,5 +236,13 @@
 		private System.Windows.Forms.DataGridView dataGridView1;
 		private System.Windows.Forms.Button buttonSearch;
 		private System.Windows.Forms.Button buttonAdd;
+		private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn accountDataGridViewTextBoxColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn passwordDataGridViewTextBoxColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn dateOfBirthDataGridViewTextBoxColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn heightDataGridViewTextBoxColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+		private System.Windows.Forms.BindingSource userEntityBindingSource;
 	}
 }
