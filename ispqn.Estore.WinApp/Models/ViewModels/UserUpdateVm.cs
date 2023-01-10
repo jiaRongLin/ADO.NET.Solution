13f73afca4814308e8625f79fe6dc3cabf2f3dad
Jia <vernalin1422@gmail.com>
2023-01-10 08:49:31 +0800

    done

--- ispqn.Estore.WinApp/Models/ViewModels/UserUpdateVm.cs
+++ ispqn.Estore.WinApp/Models/ViewModels/UserUpdateVm.cs
@@ -11,9 +11,23 @@ namespace ispqn.Estore.WinApp.Models.ViewModels
 	public class UserUpdateVm
 	{
 		public int Id { get; set; }
+
+		[Display(Name = "姓名")]
+		[Required(ErrorMessage = "{0}必填")] //必填一定要寫
+		[StringLength(50, ErrorMessage = "{0}長度不可超過{1}")]
 		public string Name { get; set; }
+
+		[Display(Name = "帳號")]
+		[Required(ErrorMessage = "{0}必填")]
+		[StringLength(50, ErrorMessage = "{0}長度不可超過{1}")]
 		public string Account { get; set; }
+
+		[Display(Name = "密碼")]
+		[Required(ErrorMessage = "{0}必填")]
+		[StringLength(50, ErrorMessage = "{0}長度不可超過{1}")]
 		public string Password { get; set; }
+
+		
 		[DataType(DataType.Date, ErrorMessage = "生日必須是合法日期，例如1990/01/09")]
 		public DateTime? DateOfBirth { get; set; }
 
